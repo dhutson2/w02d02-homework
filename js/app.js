7e73f5595ebe16c345760303ff09a73f1ec03119
Derek Hutson <denverd1301@gmail.com>
2019-06-26 19:53:01 -0600

    Finished pokemon game! With one exception.. see comment above play game function

--- js/app.js
+++ js/app.js
@@ -1,139 +1,125 @@
 console.log('go to work!')
 
-
-
-// OOO:
-
-// ## The `game` object
-
-// For each part, think about:
-// * What's the best data type for this property? Number? String? Array? object? boolean? Some crazy combination of these).  
-//   * Hint/reminder: use a property when you want to "keep track" of something
-// * Or should you create a method?
-//   * Hint/reminder: use a method when you want to "do" something
-
-// #### The game should be able to:
-
-// 1. keep a library of all the Pokemon cards that can be played (see the array in the "The Cards" section)
-// 2. know what cards have been played
-// 3. know how many cards are left to be played/dealt overall
-// 4. track points for both the player and the computer
-//     Note: Points are determined by the following: If the player's card beats the computer's card, the player gets one point (and vice versa). If there is a tie, no one gets a point.
-// 5. track rounds
-// 6. track number of rounds won for both player and computer
-// 7. automatically deal 3 cards from the library to the player and 3 cards to the computer each round
-// 8. determine the winner of each play
-// 9. stop once there are no cards left or not enough to deal 3 to each the player and computer
-
-
-// ## The `player` object
-
-// ### The player should be able to:
-
-// 1. see their stats: their points and how many rounds they've won.
-// 2. see what cards they have been dealt/see what cards are left in their hand
-// 3. pick a card from the hand that has been dealt to them (thereby playing this card agaist the computer's card). The round ends once this has happened 3 times.
-// 4. receive new cards given to them by the game each round.
-// 5. see the cards that they have played in the past.
-
-
-// ## The "UI"
-
-// The user should see the following in the console:
-
-// - the scoreboard after each round
-// - the cards in the player's hand
-// - the cards in the computer's hand
-// - the cards that are in play
-// - the winner of each round (or if there was a tie)
-// - the winner of the game when the game is over 
-// - the final score when the game is over
-
-
-
-
-
-
-
-
-
 const cardDeck = [
-    {
-      name: "Bulbasaur",
-      damage: 60
-    }, {
-      name: "Caterpie",
-      damage: 40
-    }, {
-      name: "Charmander",
-      damage: 60
-    }, {
-      name: "Clefairy",
-      damage: 50
-    }, {
-      name: "Jigglypuff",
-      damage: 60
-    }, {
-      name: "Mankey",
-      damage: 30
-    }, {
-      name: "Meowth",
-      damage: 60
-    }, {
-      name: "Nidoran - female",
-      damage: 60
-    }, {
-      name: "Nidoran - male",
-      damage: 50
-    }, {
-      name: "Oddish",
-      damage: 40
-    }, {
-      name: "Pidgey",
-      damage: 50
-    }, {
-      name: "Pikachu",
-      damage: 50
-    }, {
-      name: "Poliwag",
-      damage: 50
-    }, {
-      name: "Psyduck",
-      damage: 60
-    }, {
-      name: "Rattata",
-      damage: 30
-    }, {
-      name: "Squirtle",
-      damage: 60
-    }, {
-      name: "Vulpix",
-      damage: 50
-    }, {
-      name: "Weedle", 
-      damage: 40
-    }
-  ];
+  {
+    name: "Bulbasaur",
+    damage: 60
+  }, {
+    name: "Caterpie",
+    damage: 40
+  }, {
+    name: "Charmander",
+    damage: 60
+  }, {
+    name: "Clefairy",
+    damage: 50
+  }, {
+    name: "Jigglypuff",
+    damage: 60
+  }, {
+    name: "Mankey",
+    damage: 30
+  }, {
+    name: "Meowth",
+    damage: 60
+  }, {
+    name: "Nidoran - female",
+    damage: 60
+  }, {
+    name: "Nidoran - male",
+    damage: 50
+  }, {
+    name: "Oddish",
+    damage: 40
+  }, {
+    name: "Pidgey",
+    damage: 50
+  }, {
+    name: "Pikachu",
+    damage: 50
+  }, {
+    name: "Poliwag",
+    damage: 50
+  }, {
+    name: "Psyduck",
+    damage: 60
+  }, {
+    name: "Rattata",
+    damage: 30
+  }, {
+    name: "Squirtle",
+    damage: 60
+  }, {
+    name: "Vulpix",
+    damage: 50
+  }, {
+    name: "Weedle", 
+    damage: 40
+  }
+];
+
+const playerCards = [];
+const computerCards = [];
 
 
+const getRandomCard = (obj) => {
+  const card = obj[Math.floor(Math.random()*obj.length)];
+  return card;
+}
 
+const winCheck = () => {
+  if(player.score === 3){
+    console.log('Congrats! You are the true poke master!!')
+  } else if(computer.score === 3){
+    console.log('You still have much training to do... try again')
+  } else{
+    playGame();
+  }
+}
 
+for(let i =0; i < 3; i++){
+playerCards.push(getRandomCard(cardDeck));
+computerCards.push(getRandomCard(cardDeck));
 
-class Player {
-    
 }
 
 
+const player = {
+  name: 'D',
+  cards: playerCards,
+  score: 0,
+  playCard(){
+    let card = getRandomCard(playerCards);
+    return card
+  }
+}
 
+const computer = {
+  name: 'Ziggler',
+  cards: computerCards,
+  score: 0,
+  playCard(){
+    let card = getRandomCard(computerCards);
+    return card
+  }
+}
 
-
-
-
-
-
-
-
-
+// run play game function in console to start game... refresh browser before playing again otherwise you get endless game of pokemon!!
+const playGame = () => {
+  let playerCard = player.playCard();
+  let computerCard = computer.playCard();
+  if(playerCard.damage > computerCard.damage){
+    console.log(`You played a wild ${playerCard.name}! and computer played a wild ${computerCard.name}`)
+    player.score += 1
+    console.log(`You won this round! You now have ${player.score} points and the computer has ${computer.score} points.`)
+  } else if(playerCard.damage < computerCard.damage){
+    console.log(`Computer played a wild ${computerCard.name} and You played a wild ${playerCard.name}!`)
+    computer.score += 1
+    console.log(`You lost this round! You now have ${player.score} points and the computer has ${computer.score} points.`)
+  } else{
+    playGame();
+  } winCheck();
+}
 
 
 
